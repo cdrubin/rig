8d68d17bd1ce2cac33cfa3463c65826a6771e83b
Cefan Rubin <cefan@rubin.za.net>
2021-05-23 14:41:56 -0400

    Added status code handling on load

--- load.lua
+++ load.lua
@@ -1,3 +1,12 @@
 SetHeader( 'Access-Control-Allow-Origin', '*' )
-contents = LoadAsset( GetParam( 'filename' ) )
-Write( contents )
+
+file = io.open( EscapeHtml( GetParam( 'filename' ) ), 'r' )
+if ( file == nil ) then
+    SetStatus( 404 )
+    Write( '<h5>load.lua</h5>\n' )
+    Write( 'filename: ' .. EscapeHtml( GetParam( 'filename' ) ) .. '\n' )    
+else
+    local content = file:read("*all")
+    file:close()
+    Write( content )
+end
